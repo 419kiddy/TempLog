e152db52e0ed44cdabe23ae1706a2793d96e0001
419kiddy <kiddy_tm@fuji.waseda.jp>
2021-07-13 00:19:11 +0900

    No saisoku

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage, ConfirmTemplate, PostbackAction, MessageAction, PostbackEvent
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage, ConfirmTemplate, PostbackAction, MessageAction, PostbackEvent, ButtonsTemplate
 )
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ with open('secret.yaml', 'r') as yf:
     JSONF = cf["env_variables"]["JSONF"]
     SPREADSHEET_KEY = cf["env_variables"]["SPREADSHEET_KEY"]
 
-FOLLOW_TEXT = '初めまして。当botは毎朝8時半に体温と健康を確認するメッセージを送信します。安全な生活のためご協力お願いいたします。半角数字で体温を送信すると記録することができます。'
+FOLLOW_TEXT = '初めまして。当botは毎朝8時半に体温と健康を確認するメッセージを送信します。安全な生活のためご協力お願いいたします。\n半角数字で体温を送信後、体調不良の有無を確認します。記録には数秒かかるため連打は避けるようお願いします。'
 
 #スプレッドシートと接続
 def connect_gspread(jsonf, key):
@@ -46,6 +46,13 @@ def is_float(string):
     except ValueError:
         return False
 
+def get_health(string):
+    index = string.find("&")
+    return string[index+1:]
+
+def get_temp(string):
+    index = string.find("&")
+    return string[:index]
 
 #スプレッドシートに書き込み
 def write_temp(profile, event):
@@ -55,9 +62,10 @@ def write_temp(profile, event):
     jst_dt = utc_dt + timedelta(hours=9)
     ws.update_cell(write_point, 1, jst_dt.strftime("%Y/%m/%d"))
     ws.update_cell(write_point, 2, jst_dt.strftime("%H:%M:%S"))
-    ws.update_cell(write_point, 3, event.postback.data)
+    ws.update_cell(write_point, 3, get_temp(event.postback.data))
     ws.update_cell(write_point, 4, profile.user_id)
     ws.update_cell(write_point, 5, profile.display_name)
+    ws.update_cell(write_point, 6, get_health(event.postback.data))
 
 
 @app.route("/callback", methods=['POST'])
@@ -82,14 +90,19 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
-    confirm_template_message = TemplateSendMessage(
-        alt_text='Confirm',
-        template=ConfirmTemplate(
-            text=event.message.text+'℃を記録します。よろしいですか？',
+    buttons_template_message = TemplateSendMessage(
+        alt_text='健康観察',
+        template=ButtonsTemplate(
+            type="buttons",
+            text=event.message.text+'℃を記録します。ご自身または同居人に風邪の症状、倦怠感、息苦しさ、嗅覚・味覚異常、だるさに当てはまる症状がある、濃厚接触者がいる場合は体調不良有りを選んでください。(押してから数秒かかります。)', 
             actions=[
                 PostbackAction(
-                    label='OK',
-                    data=event.message.text
+                    label='体調不良無し',
+                    data=event.message.text+"&fine"
+                ),
+                PostbackAction(
+                    label='体調不良有り',
+                    data=event.message.text+"&sick"
                 ),
                 PostbackAction(
                     label='cancel',
@@ -98,10 +111,19 @@ def handle_message(event):
             ]
         )
     )
-    line_bot_api.reply_message(
-        event.reply_token,
-        confirm_template_message
-    )
+    if is_float(get_temp(event.message.text)) :
+        line_bot_api.reply_message(
+            event.reply_token,
+            buttons_template_message
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text="まず初めに体温を送ってください。半角数字・小数点のみ対応しています。"
+            )
+        )
+
 
 
 @handler.add(FollowEvent)
@@ -122,7 +144,7 @@ def handle_postback(event):
             )
         )
     
-    elif is_float(event.postback.data):
+    elif is_float(get_temp(event.postback.data)):
         profile = line_bot_api.get_profile(event.source.user_id)
         write_temp(profile, event)
         line_bot_api.reply_message(
